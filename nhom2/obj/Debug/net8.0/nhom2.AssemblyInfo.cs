3861b8e67d9e7332fb7f55aa6bd198b8bf2b738a
Võ Minh Quân <160707749+Huyen64@users.noreply.github.com>
2026-05-19 10:10:58 +0700

    z

--- nhom2/obj/Debug/net8.0/nhom2.AssemblyInfo.cs
+++ nhom2/obj/Debug/net8.0/nhom2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("nhom2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0e0a7527fa5a7d3ecb46f4a88b81a9c634af46e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+436932c1117f0c772b093bb2200fc0d8939bb57e")]
 [assembly: System.Reflection.AssemblyProductAttribute("nhom2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("nhom2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
