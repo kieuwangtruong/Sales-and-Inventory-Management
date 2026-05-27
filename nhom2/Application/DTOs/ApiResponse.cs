d114237c2bd33f4c97aec0b413cc744d525e8aaf
Võ Minh Quân <162551918+vminhquan@users.noreply.github.com>
2026-05-27 15:40:27 +0700

    .

--- nhom2/Application/DTOs/ApiResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace nhom2.Application.DTOs
-{
-    public class ApiResponse<T>
-    {
-        public bool Success { get; set; }
-        public T? Data { get; set; }
-        public string? Message { get; set; }
-    }
-}
